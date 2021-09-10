2ce86afdc5b33f1bd8d0f587a968fa3e171aebf0
Vittorio Corbo <32842803+vitoxio@users.noreply.github.com>
2021-09-10 12:35:20 -0400

    All changes before full revamp

--- scripts.js
+++ scripts.js
@@ -2,6 +2,16 @@ function changeFx() {
   document.getElementById("demos").innerHTML = "Paragraph changed.";
 }
 
+//HIGHLIGHT PAGE I AM ON
+  function change_text() {
+      document.getElementById("myH2").style.color = "#0000ff";
+  }
+  function old_color() {
+      //my color
+      document.getElementById("myH2").style.color = "#FFFFFF";
+      //document.getElementById("myH2").style.color = "#000f27";
+  }
+
 function gimme_pizza() {
   var pizza = "pizzaaaa";
   document.getElementById("pizzas").innerHTML = pizza;
