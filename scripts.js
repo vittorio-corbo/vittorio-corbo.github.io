89f2d6e7593f00850a068beafa20e88373e3e950
Vittorio Corbo <32842803+vitoxio@users.noreply.github.com>
2021-09-13 20:11:53 -0400

    Fix ul for sidebar

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@ function changeFx() {
   document.getElementById("demos").innerHTML = "Paragraph changed.";
 }
 
-//HIGHLIGHT PAGE I AM ON
+//HIGHLIGHT PAGE I AM ON\
+
   function change_text() {
       document.getElementById("myH2").style.color = "#0000ff";
   }
@@ -11,6 +12,7 @@ function changeFx() {
       document.getElementById("myH2").style.color = "#FFFFFF";
       //document.getElementById("myH2").style.color = "#000f27";
   }
+  
 
 function gimme_pizza() {
   var pizza = "pizzaaaa";
