e7c44e245225b696db0b48b8e2e7e9584c55d8ba
Vittorio Corbo <vittocorbo@gmail.com>
2026-03-22 17:09:36 -0400

    add basis of new pageloading system

--- scripts.js
+++ scripts.js
@@ -35,6 +35,20 @@ function showPage() {
     document.getElementById("myDiv").style.display = "block";
 }
 
+function showPage2() {
+  document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.getElementById("loader").style.display = "block";
+        document.getElementById("myDiv").style.display = "none";
+    } else {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("myDiv").style.display = "block";
+    }
+  };
+
+    
+}
+
 
 
 
